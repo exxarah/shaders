b146812ab47ebdc2e22464e37bf08f98a0d0dc7a
Exxarah <sarahbeavan@gmail.com>
2021-09-14 07:54:33 +1200

    Triangle!
    Chapter 5.5.2 completed

--- shaders_app/Window.cs
+++ shaders_app/Window.cs
@@ -4,17 +4,57 @@ using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 
 using OpenTK.Graphics.OpenGL;
+using shaders_lib;
 
 namespace shaders_app
 {
     public class Window : GameWindow
     {
+        private readonly float[] _vertices = new float[]
+        {
+            // Center of the screen is 0? How to make square filling whole screen for 2d, -1 to 1?
+            //x      y     z
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f,
+        };
+
+        private int _vertexBufferObject;
+        private int _vertexArrayObject;
+
+        private Shader _shader;
+        
         public Window(int width, int height, string title) :
             base(new GameWindowSettings(), new NativeWindowSettings() {
                 Size = new Vector2i(width, height), 
                 Title = title
             })
         { }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            // Set default clear colour
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            // Make VBO
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            
+            // Make VAO
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+            // Inform of format of data
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            // Please god someone save me from this awful path
+            _shader = new Shader("../../../shaders/shader.vert", "../../../shaders/shader.frag");
+            _shader.Use();
+        }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
@@ -30,10 +70,20 @@ namespace shaders_app
         {
             base.OnRenderFrame(args);
             
-            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             GL.Clear(ClearBufferMask.ColorBufferBit);
+            _shader.Use();
+            
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
             
             SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            
+            GL.Viewport(0, 0, Size.X, Size.Y);
         }
     }
 }
---
+++ shaders_lib/Shader.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Graphics;
+using OpenTK.Mathematics;
+
+namespace shaders_lib
+{
+    /// <summary>
+    /// Represents an OpenGL Program, consisting of a number of Shaders
+    /// </summary>
+    public class Shader
+    {
+        public readonly int Handle;
+
+        private readonly Dictionary<string, int> _uniformLocations;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Shader"/> class.
+        /// </summary>
+        /// <param name="vertPath">Path to the desired Vertex Shader</param>
+        /// <param name="fragPath">Path to the desired Fragment Shader</param>
+        public Shader(string vertPath, string fragPath)
+        {
+            var shaderSource = File.ReadAllText(vertPath);
+            var vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, shaderSource);
+            CompileShader(vertexShader);
+
+            shaderSource = File.ReadAllText(fragPath);
+            var fragShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragShader, shaderSource);
+            CompileShader(fragShader);
+
+            Handle = GL.CreateProgram();
+            GL.AttachShader(Handle, vertexShader);
+            GL.AttachShader(Handle, fragShader);
+            LinkProgram(Handle);
+            
+            GL.DetachShader(Handle, vertexShader);
+            GL.DetachShader(Handle, fragShader);
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragShader);
+            
+            GL.GetProgram(Handle, GetProgramParameterName.ActiveUniforms, out var numberOfUniforms);
+            _uniformLocations = new Dictionary<string, int>();
+            for (int i = 0; i < numberOfUniforms; i++)
+            {
+                var key = GL.GetActiveUniform(Handle, i, out _, out _);
+                var location = GL.GetUniformLocation(Handle, key);
+                _uniformLocations.Add(key, location);
+            }
+        }
+
+        /// <summary>
+        /// Wraps GL.CompileShader() to include error checking and reporting
+        /// </summary>
+        /// <param name="shader">The shader to compile</param>
+        /// <exception cref="Exception">ShaderInfoLog, in case of compile failure</exception>
+        private static void CompileShader(int shader)
+        {
+            GL.CompileShader(shader);
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out var code);
+            if (code != (int)All.True)
+            {
+                var infolog = GL.GetShaderInfoLog(shader);
+                throw new Exception($"Error occurred while compiling Shader({shader}).\n\n{infolog}");
+            }
+        }
+
+        /// <summary>
+        /// Wraps GL.LinkProgram() to include error checking and reporting
+        /// </summary>
+        /// <param name="program">The program to link</param>
+        /// <exception cref="Exception">ProgramInfoLog, in case of link failure</exception>
+        private static void LinkProgram(int program)
+        {
+            GL.LinkProgram(program);
+            GL.GetProgram(program, GetProgramParameterName.LinkStatus, out var code);
+            if (code != (int)All.True)
+            {
+                var infolog = GL.GetProgramInfoLog(program);
+                throw new Exception($"Error occurred while compiling Program({program}).\n\n{infolog}");
+            }
+        }
+
+        /// <summary>
+        /// Wraps GL.UseProgram to enable easy object-oriented use and access
+        /// </summary>
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+
+        /// <summary>
+        /// Wraps GL.GetAttribLocation to enable easy object-oriented use and access
+        /// </summary>
+        public int GetAttribLocation(string attribName)
+        {
+            return GL.GetAttribLocation(Handle, attribName);
+        }
+
+        /// <summary>
+        /// Set a uniform int on this shader
+        /// </summary>
+        /// <param name="name">The name of the uniform</param>
+        /// <param name="data">The data to be set</param>
+        public void SetInt(string name, int data)
+        {
+            GL.UseProgram(Handle);
+            GL.Uniform1(_uniformLocations[name], data);
+        }
+        
+        /// <summary>
+        /// Set a uniform float on this shader
+        /// </summary>
+        /// <param name="name">The name of the uniform</param>
+        /// <param name="data">The data to be set</param>
+        public void SetFloat(string name, float data)
+        {
+            GL.UseProgram(Handle);
+            GL.Uniform1(_uniformLocations[name], data);
+        }
+        
+        /// <summary>
+        /// Set a uniform Matrix4 on this shader
+        /// </summary>
+        /// <param name="name">The name of the uniform</param>
+        /// <param name="data">The data to be set</param>
+        public void SetMatrix4(string name, Matrix4 data)
+        {
+            GL.UseProgram(Handle);
+            GL.UniformMatrix4(_uniformLocations[name], true, ref data);
+        }
+        
+        /// <summary>
+        /// Set a uniform int on this shader
+        /// </summary>
+        /// <param name="name">The name of the uniform</param>
+        /// <param name="data">The data to be set</param>
+        public void SetVector3(string name, Vector3 data)
+        {
+            GL.UseProgram(Handle);
+            GL.Uniform3(_uniformLocations[name], data);
+        }
+    }
+}
