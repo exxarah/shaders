8b022e299c82d4a96b4d04c6c20174684618c4a4
Exxarah <sarahbeavan@gmail.com>
2021-10-03 12:07:20 +1300

    Fixed the cursed paths Added directory copying to the csproj, also gives us a handy Assets folder to use for any materials, etc

---
+++ shaders_lib/Assets/glsl/static.frag
@@ -0,0 +1,9 @@
+﻿#version 330 core
+
+in vec3 colour;
+
+out vec4 diffuseColor;
+
+void main() {
+    diffuseColor = vec4(colour, 1.0);
+}
--- shaders_lib/Shaders/StaticShader.cs
+++ shaders_lib/Shaders/StaticShader.cs
@@ -4,9 +4,8 @@ namespace shaders_lib.Shaders
 {
     public class StaticShader : Shader
     {
-        // Please god someone save me from this awful path
-        private static readonly string VertexPath = "../../../shaders/" + "shader" + ".vert";
-        private static readonly string FragmentPath = "../../../shaders/" + "shader" + ".frag";
+        private static readonly string VertexPath = "Assets/glsl/static.vert";
+        private static readonly string FragmentPath = "Assets/glsl/static.frag";
         public StaticShader() : base(VertexPath, FragmentPath) { }
         
         protected override void BindAttributes()
